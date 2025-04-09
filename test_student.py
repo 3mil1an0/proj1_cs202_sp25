e3253478db403c8e43d0aa010814bf2a8d5984af
KirkADuran <kirkaduran@gmail.com>
2025-04-09 14:30:34 -0700

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -1,6 +1,8 @@
 import unittest
 from proj1 import *
 #proj1.py should contain your data class and function definitions
+#these do not contribute positivly to your grade. 
+#but your grade will be lowered if they are missing
 
 class TestRegionFunctions(unittest.TestCase):
 
